fcfe12efd76176752f3e71ae889c9c82fd5415de
huoapeng <huoapeng@gmail.com>
2016-04-22 01:54:02 +0800

    get user by marshal_with

--- myapi/__init__.py
+++ myapi/__init__.py
@@ -15,30 +15,8 @@ def create_app(config_name):
 	from myapi.resources.user import User
 	from myapi.resources.general import general
 	api = restful.Api(app)
-	api.add_resource(User, '/user', '/user/<string:todo_id>')
+	api.add_resource(User, '/user', '/user/<string:userid>')
 	api.add_resource(general, '/general/<string:method>')
 
 	return app
 
-# @app.teardown_appcontext
-# def shutdown_session(exception=None):
-#     db_session.remove()
-
-# @app.errorhandler(404)
-# def not_found(error):
-#     return render_template('404.html'), 404
-
-# @app.before_request
-# def load_current_user():
-#     g.user = User.query.filter_by(openid=session['openid']).first() \
-#         if 'openid' in session else None
-
-# from myapi.database import db_session
-
-# @app.teardown_request
-# def remove_db_session(exception):
-#     db_session.remove()
-
-# @app.context_processor
-# def current_year():
-#     return {'current_year': datetime.utcnow().year}
--- myapi/resources/user.py
+++ myapi/resources/user.py
@@ -8,47 +8,72 @@ post_parser = reqparse.RequestParser()
 post_parser.add_argument(
     'email', dest='email',
     type=str, location='form',
-    required=True, help='Missing required parameter in the JSON body ,The user\'s email',
+    required=True, help='Missing required parameter in the JSON body ,The user\'s email'
 )
 post_parser.add_argument(
     'password', dest='password',
     type=str, 
-    required=True, help='The user\'s password',
+    required=True, help='The user\'s password'
+)
+post_parser.add_argument(
+    'type', dest='type',
+    type=int, 
+    required=True,
+    choices=range(2)
 )
 # post_parser.add_argument(
 #     'user_priority', dest='user_priority',
 #     type=int, location='form',
 #     default=1, choices=range(5), help='The user\'s priority',
 # )
-
+usersss = {
+    'id':1,
+    'email':'123123',
+    'password':'dfsdfds'
+}
 user_fields = {
     'id': fields.Integer,
-    'username': fields.String(default='Anonymous User'),
+    # 'username': fields.String(default='Anonymous User'),
     'email': fields.String,
-    'user_priority': fields.Integer,
-    'custom_greeting': fields.FormattedString('Hey there {username}!'),
-    'date_created': fields.DateTime,
-    'date_updated': fields.DateTime,
-    'links': fields.Nested({
-        'friends': fields.Url('/Users/{id}/Friends'),
-        'posts': fields.Url('Users/{id}/Posts'),
-    }),
+    'password': fields.String
+    # 'user_priority': fields.Integer,
+    # 'custom_greeting': fields.FormattedString('Hey there {username}!'),
+    # 'date_created': fields.DateTime,
+    # 'date_updated': fields.DateTime,
+    # 'links': fields.Nested({
+    #     'friends': fields.Url('/Users/{id}/Friends'),
+    #     'posts': fields.Url('Users/{id}/Posts'),
+    # }),
 }
 
 
 class User(Resource):
-    def get(self, todo_id= None):
-        return {'result':'true'}
+    @marshal_with(user_fields)
+    def get(self, userid):
+        return UserModel.query.get(userid)
+        #return UserModel.query.filter_by(id=userid).first()
+        #todos=Todo.query.order_by(Todo.pub_date.desc()).all()
 
     def post(self):
         args = post_parser.parse_args()
         if not valid_email(args.email,):
             return {'status':'False','message':'pls check email'}
         else:
-            u = UserModel(args.email, args.password)
-            db.session.add(u)
-            db.session.commit()
-            return {'status':'True','message':'regist successfully'}
+            if args.type == 0:
+                user = UserModel.query.filter_by(email=args.email).first()
+                if user is None:
+                    u = UserModel(args.email, args.password)
+                    db.session.add(u)
+                    db.session.commit()
+                    return {'status':'True','message':'regist successfully'}
+                else:
+                    return {'status':'False','message':'account is already exist'}
+            else:
+                user = UserModel.query.filter_by(email=args.email).filter_by(password=args.password).first()
+                if user is None:
+                    return {'status':'False','message':'account is not exist'}
+                else:
+                    return {'status':'True','message':'account is logon'}
 
     def put(self):
         pass
@@ -56,34 +81,7 @@ class User(Resource):
     def delete(self):
         pass
 
-# @app.route('/')
-# def show_all():
-#     return render_template('show_all.html',
-#         todos=Todo.query.order_by(Todo.pub_date.desc()).all()
-#     )
-
-
-# @app.route('/new', methods=['GET', 'POST'])
-# def new():
-#     if request.method == 'POST':
-#         if not request.form['title']:
-#             flash('Title is required', 'error')
-#         elif not request.form['text']:
-#             flash('Text is required', 'error')
-#         else:
-#             todo = Todo(request.form['title'], request.form['text'])
-#             db.session.add(todo)
-#             db.session.commit()
-#             flash(u'Todo item was successfully created')
-#             return redirect(url_for('show_all'))
-#     return render_template('new.html')
-
-
-# @app.route('/update', methods=['POST'])
-# def update_done():
 #     for todo in Todo.query.all():
 #         todo.done = ('done.%d' % todo.id) in request.form
 #     flash('Updated status')
 #     db.session.commit()
-#     return redirect(url_for('show_all'))
-
