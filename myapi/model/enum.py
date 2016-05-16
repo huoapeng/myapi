5dc76064b32040c70a2ddf02ac146a971354cfaa
huoapeng <huoapeng@gmail.com>
2016-05-16 12:21:28 +0800

    add task resource

--- myapi/model/enum.py
+++ myapi/model/enum.py
@@ -1,6 +1,6 @@
-class user_actions:
-    regist = 0
-    login = 1
+# class user_actions:
+#     regist = 0
+#     login = 1
 
 class user_status:
     normal = 0
@@ -12,7 +12,12 @@ class project_status:
 
 class task_status:
     normal = 0
+    delete = 1
 
 class message_type:
     version = 0
     note = 1
+
+class type_status:
+    normal = 0
+    delete = 1
--- myapi/model/task.py
+++ myapi/model/task.py
@@ -4,22 +4,22 @@ from types import task_types
 
 class TaskModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Integer)
     name = db.Column(db.String(100))
+    # status = db.Column(db.Integer)
     timespan = db.Column(db.Integer)
     requirements = db.Column(db.String(100))
-    reward = db.Column(db.Integer)
+    bonus = db.Column(db.Integer)
     description = db.Column(db.String(5000))
-    publishDate = db.Column(db.DateTime)
+    # publishDate = db.Column(db.DateTime)
     bidder_qualification_requirement = db.Column(db.String(100))
     bidder_area_requirement = db.Column(db.String(100))
 
-    types = db.relationship('TypeModel', secondary=task_types,
-        backref=db.backref('tasks', lazy='dynamic'))
-
     project_id = db.Column(db.Integer, db.ForeignKey('project_model.id'))
     # owner_id = db.Column(db.Integer, db.ForeignKey('user_model.id'))
-    bidder_successful = db.Column(db.Integer, db.ForeignKey('user_model.id'))
+    bidder_winner = db.Column(db.Integer, db.ForeignKey('user_model.id'))
+
+    types = db.relationship('TypeModel', secondary=task_types,
+        backref=db.backref('tasks', lazy='dynamic'))
 
     # user = relationship('User', foreign_keys='Friend.user_id')
     # friend = relationship('User', foreign_keys='Friend.friend_id')
@@ -27,15 +27,26 @@ class TaskModel(db.Model):
     versions = db.relationship('VersionModel',
         backref=db.backref('task', lazy='joined'), lazy='dynamic')
 
+
     notes = db.relationship('NoteModel',
         backref=db.backref('task', lazy='joined'), lazy='dynamic')
 
-    bidders = db.relationship('UserModel',
-        backref=db.backref('bidde_tasks', lazy='joined'), lazy='joined')
+    # bidders = db.relationship('UserModel',
+    #     backref=db.backref('bidde_tasks', lazy='joined'), lazy='joined')
 
-    def __init__(self, taskName):
-        self.name = taskName
+    def __init__(self, name, timespan, requirements, bonus, description, 
+        bidder_qualification_requirement, bidder_area_requirement, project_id, bidder_winner):
+        self.name = name
         self.status = task_status.normal
+        self.timespan = timespan
+        self.requirements = requirements
+        self.bonus = bonus
+        self.description = description
+        # self.publishDate = publishDate
+        self.bidder_qualification_requirement = bidder_qualification_requirement
+        self.bidder_area_requirement = bidder_area_requirement
+        self.project_id = project_id
+        self.bidder_winner = bidder_winner
 
     def __repr__(self):
         return '<User %r>' % (self.name)
