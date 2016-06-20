1d03b1e0377bd630992e4c4911c3643d685f1902
huoapeng <huoapeng@gmail.com>
2016-06-20 16:36:50 +0800

    fix tag bug

--- myapi/model/tag.py
+++ myapi/model/tag.py
@@ -14,6 +14,12 @@ class UserTagModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
 
+    def __init__(self, name):
+		self.name = name
+
 class WorkTagModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
+    
+    def __init__(self, name):
+		self.name = name
