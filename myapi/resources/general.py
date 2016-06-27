ea62c044e06d91c50a71492470e0ef36a10aed9c
huoapeng <huoapeng@gmail.com>
2016-06-27 20:03:47 +0800

    refactor file and image upload params

--- myapi/resources/general.py
+++ myapi/resources/general.py
@@ -14,26 +14,27 @@ from myapi.model.kind import KindModel
 from myapi.common.image import resize, allowedFile, getServerPath
 
 class image(Resource):
-    def get(self, userid, imagetype, filename):
-        if filename:
-            fpath = os.path.join(app.config['ROOT_PATH'], app.config['UPLOAD_FOLDER'], path[imagetype](userid))
-            return send_from_directory(fpath, filename)
-        else:
-            return ''
+    # def get(self, foldername, imagetype, filename):
+    #     if filename:
+    #         fpath = os.path.join(app.config['ROOT_PATH'], 
+    #             app.config['UPLOAD_FOLDER'], path[imagetype](foldername))
+    #         return send_from_directory(fpath, filename)
+    #     else:
+    #         return ''
 
     def post(self):
         file = request.files['file']
         get_parser = reqparse.RequestParser()
         get_parser.add_argument('type', type=int, location='args', choices=range(1, 10), required=True)
-        get_parser.add_argument('userid', type=int, location='args', required=True)
+        get_parser.add_argument('foldername', type=int, location='args', required=True)
         get_parser.add_argument('thumbnail', type=int, location='args', default=0)
         args = get_parser.parse_args()
 
         if file and allowedFile(args.type, file.filename):
-            sf = getServerPath(args.type, args.userid, file.filename)
+            sf = getServerPath(args.type, args.foldername, file.filename)
 
             if args.type == file_type.profile:
-                user = UserModel.query.get(args.userid)
+                user = UserModel.query.get(args.foldername)
                 user.image = os.path.basename(sf)
                 db.session.commit()
                 file = resize(file, 100, 80)
@@ -42,7 +43,7 @@ class image(Resource):
 
             if args.thumbnail:
                 thumbnailfile = resize(file, 223, 99999)
-                tsf = getServerPath(file_type.workThumbnail, args.userid, file.filename)
+                tsf = getServerPath(file_type.workThumbnail, args.foldername, file.filename)
                 thumbnailfile.save(tsf)
                 return jsonify(image=os.path.basename(sf), thumbnail=os.path.basename(tsf))
             else:
@@ -54,11 +55,11 @@ class CompressFile(Resource):
         file = request.files['file']
         get_parser = reqparse.RequestParser()
         get_parser.add_argument('type', type=int, location='args', choices=range(51, 52), required=True)
-        get_parser.add_argument('userid', type=int, location='args', required=True)
+        get_parser.add_argument('foldername', type=int, location='args', required=True)
         args = get_parser.parse_args()
 
         if file and allowedFile(args.type, file.filename):
-            sf = getServerPath(file.filename, args.type, args.userid)
+            sf = getServerPath(args.type, args.foldername, file.filename)
             file.save(sf)
             return jsonify(data=os.path.basename(sf))
         return 'pls check file suffix'
