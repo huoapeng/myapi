f2b3dad25b803c7e625f53303754fe5a28efecf9
huoapeng <huoapeng@gmail.com>
2016-06-30 18:47:09 +0800

    try debug on remote server

--- run.py
+++ run.py
@@ -2,6 +2,4 @@ from myapi import create_app
 
 if __name__ == '__main__':
     app = create_app('config.dev')
-    # app = create_app('config.webapiconfig')
-    # app.app_context().push()
-    app.run(debug= app.config['DEBUG'])
+    app.run(debug=True)
