80a87404585e1c109e86bd4d32e1029402ae658f
huoapeng <huoapeng@gmail.com>
2016-07-06 17:53:42 +0800

    separate image from general resource

---
+++ myapi/resources/image.py
@@ -0,0 +1,59 @@
+import os
+from flask import request, jsonify#, send_from_directory
+from flask.ext.restful import Resource, reqparse
+# from werkzeug.datastructures import FileStorage
+from myapi import db#, app
+from myapi.model.enum import file_type
+from myapi.model.user import UserModel
+from myapi.common.image import resize, allowedFile, getServerPath
+
+class image(Resource):
+    # def get(self, foldername, imagetype, filename):
+    #     if filename:
+    #         fpath = os.path.join(app.config['ROOT_PATH'], 
+    #             app.config['UPLOAD_FOLDER'], path[imagetype](foldername))
+    #         return send_from_directory(fpath, filename)
+    #     else:
+    #         return ''
+
+    def post(self):
+        file = request.files['file']
+        get_parser = reqparse.RequestParser()
+        get_parser.add_argument('type', type=int, location='args', choices=range(1, 10), required=True)
+        get_parser.add_argument('foldername', type=int, location='args', required=True)
+        get_parser.add_argument('thumbnail', type=int, location='args', default=0)
+        args = get_parser.parse_args()
+
+        if file and allowedFile(args.type, file.filename):
+            sf = getServerPath(args.type, args.foldername, file.filename)
+
+            if args.type == file_type.profile:
+                user = UserModel.query.get(args.foldername)
+                user.image = os.path.basename(sf)
+                db.session.commit()
+                file = resize(file, 100, 80)
+
+            file.save(sf)
+
+            if args.thumbnail:
+                thumbnailfile = resize(file, 223, 99999)
+                tsf = getServerPath(file_type.workThumbnail, args.foldername, file.filename)
+                thumbnailfile.save(tsf)
+                return jsonify(image=os.path.basename(sf), thumbnail=os.path.basename(tsf))
+            else:
+                return jsonify(data=os.path.basename(sf))
+        return 'pls check file suffix'
+
+class CompressFile(Resource):
+    def post(self):
+        file = request.files['file']
+        get_parser = reqparse.RequestParser()
+        get_parser.add_argument('type', type=int, location='args', choices=range(51, 52), required=True)
+        get_parser.add_argument('foldername', type=int, location='args', required=True)
+        args = get_parser.parse_args()
+
+        if file and allowedFile(args.type, file.filename):
+            sf = getServerPath(args.type, args.foldername, file.filename)
+            file.save(sf)
+            return jsonify(data=os.path.basename(sf))
+        return 'pls check file suffix'
