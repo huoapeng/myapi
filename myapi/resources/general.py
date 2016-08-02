82d60973d9eabd532a3f7043ada6ac305a17c123
huoapeng <huoapeng@gmail.com>
2016-08-02 15:16:01 +0800

    去掉初始化大厅相关推荐和分类

--- myapi/resources/general.py
+++ myapi/resources/general.py
@@ -14,31 +14,6 @@ class general(Resource):
     def get(self, method = None):
         if method == 'create_all':
             db.create_all()
-            
-            kind1 = KindModel.query.filter_by(name = '影视大厅').first()
-            if not kind1:
-                kind1 = KindModel('影视大厅')
-                db.session.add(kind1)
-                db.session.commit()
-
-            kind11 = RecommendTypeModel.query.filter_by(name = '影视大厅').first()
-            if not kind11:
-                kind11 = RecommendTypeModel('影视大厅')
-                db.session.add(kind11)
-                db.session.commit()
-
-            kind2 = KindModel.query.filter_by(name = 'VR/AR大厅').first()
-            if not kind2:
-                kind2 = KindModel('VR/AR大厅')
-                db.session.add(kind2)
-                db.session.commit()
-
-            kind22 = RecommendTypeModel.query.filter_by(name = 'VR/AR大厅').first()
-            if not kind22:
-                kind22 = RecommendTypeModel('VR/AR大厅')
-                db.session.add(kind22)
-                db.session.commit()
-
         elif method == 'drop_all':
             db.drop_all()
         else:
