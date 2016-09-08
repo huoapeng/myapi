398a4327303253405c21227522798dfbab6ca17b
huoapeng <huoapeng@gmail.com>
2016-09-08 15:04:03 +0800

    修改静态文件返回IP地址错误，配置文件里应加正确的端口号

--- config/test.py
+++ config/test.py
@@ -2,4 +2,4 @@ SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/mydatabase'
 DEBUG = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 ROOT_PATH = '/usr/share/nginx/html/'
-IP_ADDRESS = '10.0.1.122'
+IP_ADDRESS = '10.0.1.122:8080'
