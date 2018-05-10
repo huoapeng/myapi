b228b46520bec25d75a082c5d0e4850764cfd5b6
kent <huoapeng@hotmail.com>
2018-05-10 15:54:10 +0800

    更新服务端配置文件

--- config/webapiconfig.py
+++ config/webapiconfig.py
@@ -1,7 +1,7 @@
 # SQLALCHEMY_DATABASE_URI = 'mysql://juxing:hiPpo111111@rm-uf6o082066v59n9sv.mysql.rds.aliyuncs.com:3306/hippo'
-SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/mydatabase'
+SQLALCHEMY_DATABASE_URI = 'mysql://root:hippo@localhost/mydatabase'
 DEBUG = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 ROOT_PATH = '/usr/share/nginx/html/'
-IP_ADDRESS = '139.196.183.6:8080'
+IP_ADDRESS = 'www.juxingcloud.com:8080'
 # SERVER_NAME = '139.196.183.6'
